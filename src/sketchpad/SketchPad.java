7bfdcf718104ff0d2df01b0268c578f2a6724502
Yasir Ridsan <102452106+ridsan-96@users.noreply.github.com>
2022-12-03 14:39:03 +0000

    Initial commit

---
+++ src/sketchpad/SketchPad.java
@@ -0,0 +1,18 @@
+package sketchpad;
+
+import buttons.*;
+import tools.*;
+
+public class SketchPad {
+    private final Screen screen = new Screen();
+    private final MouseInfo mouseInfo = new MouseInfo();
+    private final PaintingTool paint = new PaintingTool();
+
+    public void initialLaunch() {
+
+    }
+
+    public void update() {
+
+    }
+}
